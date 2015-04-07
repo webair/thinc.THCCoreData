e4e92c983fe73e4e9e8798b83724c32aac1180a0
u217826 <christopher.weber@sbb.ch>
2015-04-07 09:08:31 +0200

    removed default manager

--- THCCoreData/Source/manager/ContextManager.swift
+++ THCCoreData/Source/manager/ContextManager.swift
@@ -13,48 +13,6 @@ public class ContextManager {
     
     /// The Main managed object context
     public let mainContext: NSManagedObjectContext
-    
-    class var defaultSqliteURL: NSURL! {
-        get{
-            let fileManager = NSFileManager.defaultManager()
-            let coreDataDirectory = fileManager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains:NSSearchPathDomainMask.UserDomainMask).last!.URLByAppendingPathComponent("THCCoreData")
-            var error: NSError?
-            fileManager.createDirectoryAtURL(coreDataDirectory, withIntermediateDirectories: true, attributes: nil, error: &error)
-            if error != nil {
-                NSException.raise("Exception", format:"Can't create CoreData folder: %@", arguments:getVaList([error!]))
-            }
-            return coreDataDirectory.URLByAppendingPathComponent("CoreData.sqlite")
-        }
-    }
-    
-    /// The default manager, will create a SQLite persistent store database and is based on the object model from the main bundle. The sqlite database will be placed in the folder 'THCCoreData' in the documents directory
-    public class var defaultManager: ContextManager {
-        struct Static {
-            static var instance: ContextManager?
-            static var token: dispatch_once_t = 0
-        }
-        
-        dispatch_once(&Static.token) {
-            var objectModel = NSManagedObjectModel.mergedModelFromBundles([NSBundle.mainBundle()])
-            if objectModel == nil {
-                NSException(name: "Exception", reason: "Could not create object model, maby object file in bundle is missing", userInfo: nil).raise()
-            }
-            let coordinator = NSPersistentStoreCoordinator(managedObjectModel: objectModel!)
-            
-            let options = [NSMigratePersistentStoresAutomaticallyOption : true,
-                NSInferMappingModelAutomaticallyOption : true]
-            
-            var error: NSError?
-            coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: ContextManager.defaultSqliteURL, options: options, error: nil)
-            if error != nil {
-                NSException.raise("Exception", format:"Can't add default store to coordinator: %@", arguments:getVaList([error!]))
-            }
-            
-            
-            Static.instance = ContextManager(persistanceStoreCoordinator: coordinator)
-        }
-        return Static.instance!
-    }
 
     public init(persistanceStoreCoordinator: NSPersistentStoreCoordinator) {
         self.writerContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.PrivateQueueConcurrencyType)
