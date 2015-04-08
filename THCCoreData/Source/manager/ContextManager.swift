837347db0a9e939330fa6b21a87f075e7f2ed820
u217826 <christopher.weber@sbb.ch>
2015-04-08 18:48:03 +0200

    added CoreDataConfiguration class

--- THCCoreData/Source/manager/ContextManager.swift
+++ THCCoreData/Source/manager/ContextManager.swift
@@ -6,6 +6,39 @@
 import Foundation
 import CoreData
 
+public struct CoreDataConfiguration {
+    
+    public static var defaultStoreName = "CoreData.sqlite"
+    public static var defaultManagedObjectModel: NSManagedObjectModel?
+    
+    public static var defaultConfiguration: CoreDataConfiguration {get{
+        let documentsDir = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).last as NSURL
+        let rootFolder = documentsDir.URLByAppendingPathComponent("THCCoreData")
+        var error: NSError?
+        NSFileManager.defaultManager().createDirectoryAtURL(rootFolder, withIntermediateDirectories: true, attributes: nil, error: &error)
+        if error != nil {
+            NSException(name: "Exception", reason: "can't create folder in documents", userInfo: nil).raise()
+        }
+        var managedObjectModel: NSManagedObjectModel?
+        if CoreDataConfiguration.defaultManagedObjectModel != nil {
+            managedObjectModel = defaultManagedObjectModel
+        } else {
+            managedObjectModel = NSManagedObjectModel.mergedModelFromBundles([NSBundle.mainBundle()])!;
+        }
+        return CoreDataConfiguration(storeURL: rootFolder.URLByAppendingPathComponent(defaultStoreName), managedObjectModel:managedObjectModel!)
+        }
+    }
+    
+    public init (storeURL: NSURL, managedObjectModel: NSManagedObjectModel) {
+        self.storeURL = storeURL
+        self.managedObjectModel = managedObjectModel
+    }
+    
+    public var storeURL: NSURL
+    public var managedObjectModel: NSManagedObjectModel
+    
+}
+
 public class ContextManager {
     
     
@@ -13,6 +46,17 @@ public class ContextManager {
     
     /// The Main managed object context
     public let mainContext: NSManagedObjectContext
+    
+    public convenience init(configuration: CoreDataConfiguration) {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: configuration.managedObjectModel)
+        let options = [NSMigratePersistentStoresAutomaticallyOption : true,NSInferMappingModelAutomaticallyOption : true]
+        var error: NSError?
+        persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: configuration.storeURL, options: options, error: &error)
+        if error != nil {
+            NSException(name: "Exception", reason: "can't create sqliteStore at URL \(configuration.storeURL)", userInfo: nil).raise()
+        }
+        self.init(persistanceStoreCoordinator: persistentStoreCoordinator)
+    }
 
     public init(persistanceStoreCoordinator: NSPersistentStoreCoordinator) {
         self.writerContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.PrivateQueueConcurrencyType)
--- THCCoreDataTests/Source/manager/TestContextManager.swift
+++ THCCoreDataTests/Source/manager/TestContextManager.swift
@@ -11,6 +11,20 @@ import XCTest
 import THCCoreData
 import CoreData
 
+class TestCoreDataConfiguration: XCTestCase {
+    func testDefaultConfiguration() {
+        let objectModel = NSManagedObjectModel()
+        CoreDataConfiguration.defaultManagedObjectModel = objectModel
+        CoreDataConfiguration.defaultStoreName = "Test.sqlite"
+        let defaultConfig = CoreDataConfiguration.defaultConfiguration
+        let documentsDir = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).last as NSURL
+        let storeURL = documentsDir.URLByAppendingPathComponent("THCCoreData").URLByAppendingPathComponent("Test.sqlite")
+        
+        XCTAssertEqual(storeURL, defaultConfig.storeURL)
+        XCTAssertEqual(objectModel, defaultConfig.managedObjectModel)
+    }
+}
+
 class TestContextManager: XCTestCase {
 
     func testInitializeContextManager() {
@@ -21,6 +35,17 @@ class TestContextManager: XCTestCase {
         XCTAssertEqual(NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType, manager.mainContext.concurrencyType)
         XCTAssertNotNil(manager.mainContext.parentContext)
         XCTAssertEqual(NSManagedObjectContextConcurrencyType.PrivateQueueConcurrencyType, manager.mainContext.parentContext!.concurrencyType)
+    }
+    
+    func testInitializeWithConfiguration() {
+        let documentsDir = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).last as NSURL
+        let storeURL = documentsDir.URLByAppendingPathComponent("CoreData.sqlite")
+        let objectModel = NSManagedObjectModel()
+        let config = CoreDataConfiguration(storeURL: storeURL, managedObjectModel: objectModel)
+        let manager = ContextManager(configuration: config)
+        XCTAssertEqual(objectModel, manager.mainContext.persistentStoreCoordinator!.managedObjectModel)
+        let persitentStore = manager.mainContext.persistentStoreCoordinator!.persistentStores[0] as NSPersistentStore
+        XCTAssertEqual(storeURL, persitentStore.URL!)
     }
     
     func testPrivateContext() {
